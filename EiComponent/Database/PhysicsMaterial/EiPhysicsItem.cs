61e16b23262fb384ee5006b21f584247209de0d6
Eitrum <sebbbe97@hotmail.com>
2018-08-06 21:14:42 +0200

    Physics Item/material start

---
+++ EiComponent/Database/PhysicsMaterial/EiPhysicsItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eitrum
+{
+	public class EiPhysicsItem : EiScriptableObject<EiPhysicsItem>
+	{
+		#region Variables
+
+		public PhysicMaterial physicMaterial = null;
+		public float materialStrength = 1f;
+
+		#endregion
+
+		#region Properties
+
+		public PhysicMaterial PhysicMaterial
+		{
+			get
+			{
+				return physicMaterial;
+			}
+		}
+
+		public float MaterialStrength
+		{
+			get
+			{
+				return materialStrength;
+			}
+		}
+
+		#endregion
+	}
+}
