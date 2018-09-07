2251d9ed99b81679cca0a31019e3788d4d25ecc7
Eitrum <sebbbe97@hotmail.com>
2018-09-07 21:44:21 +0200

    Demo files now moved under "Eitrum.Demo"

--- EiComponent/Demo/Tasks/CubeRotation.cs
+++ EiComponent/Demo/Tasks/CubeRotation.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Eitrum;
 
-public class CubeRotation : EiComponent
-{
+namespace Eitrum.Demo {
+	public class CubeRotation : EiComponent {
 
-	void Awake ()
-	{
-		SubscribeUpdate ();
-	}
+		void Awake() {
+			SubscribeUpdate();
+		}
 
-	public override void UpdateComponent (float time)
-	{
-		this.transform.Rotate (new Vector3 (45, 0) * time);
+		public override void UpdateComponent(float time) {
+			this.transform.Rotate(new Vector3(45, 0) * time);
+		}
 	}
 }
--- EiComponent/Demo/Tasks/TaskMeshGeneration.cs
+++ EiComponent/Demo/Tasks/TaskMeshGeneration.cs
@@ -3,53 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using Eitrum;
 
-public class TaskMeshGeneration : EiComponent
-{
-	[Header ("Press Space To Generate")]
-	public MeshFilter meshFilter;
-	public int sleepTimeInMs = 1000;
-	bool isRunning = false;
+namespace Eitrum.Demo {
+	public class TaskMeshGeneration : EiComponent {
+		[Header("Press Space To Generate")]
+		public MeshFilter meshFilter;
+		public int sleepTimeInMs = 1000;
+		bool isRunning = false;
 
-	void Awake ()
-	{
-		SubscribeUpdate ();
-	}
-
-	public override void UpdateComponent (float time)
-	{
-		if (!isRunning && Input.GetKeyDown (KeyCode.Space)) {
-			isRunning = true;
-			EiTask.Run (GenerateMesh, null, ApplyMesh);
+		void Awake() {
+			SubscribeUpdate();
 		}
-	}
 
-	public Vector3[] GenerateMesh ()
-	{
-		Vector3[] verticies = new Vector3[4];
-		verticies [0] = new Vector3 (-1, 1, 0);
-		verticies [1] = new Vector3 (1, 1, 0);
-		verticies [2] = new Vector3 (1, -1, 0);
-		verticies [3] = new Vector3 (-1, -1, 0);
+		public override void UpdateComponent(float time) {
+			if (!isRunning && Input.GetKeyDown(KeyCode.Space)) {
+				isRunning = true;
+				EiTask.Run(GenerateMesh, null, ApplyMesh);
+			}
+		}
 
-		System.Threading.Thread.Sleep (sleepTimeInMs);
+		public Vector3[] GenerateMesh() {
+			Vector3[] verticies = new Vector3[4];
+			verticies[0] = new Vector3(-1, 1, 0);
+			verticies[1] = new Vector3(1, 1, 0);
+			verticies[2] = new Vector3(1, -1, 0);
+			verticies[3] = new Vector3(-1, -1, 0);
 
-		return verticies;
-	}
+			System.Threading.Thread.Sleep(sleepTimeInMs);
 
-	void ApplyMesh (Vector3[] verticies)
-	{
-		Mesh m = new Mesh ();
-		m.name = "Hello Mesh";
-		int[] n = new int[6]{ 0, 1, 2, 2, 3, 0 };
+			return verticies;
+		}
 
-		m.vertices = verticies;
-		m.triangles = n;
-		m.RecalculateNormals ();
-		m.RecalculateBounds ();
+		void ApplyMesh(Vector3[] verticies) {
+			Mesh m = new Mesh();
+			m.name = "Hello Mesh";
+			int[] n = new int[6] { 0, 1, 2, 2, 3, 0 };
 
-		if (meshFilter.mesh != null)
-			Destroy (meshFilter.mesh);
-		meshFilter.mesh = m;
-		isRunning = false;
+			m.vertices = verticies;
+			m.triangles = n;
+			m.RecalculateNormals();
+			m.RecalculateBounds();
+
+			if (meshFilter.mesh != null)
+				Destroy(meshFilter.mesh);
+			meshFilter.mesh = m;
+			isRunning = false;
+		}
 	}
 }
