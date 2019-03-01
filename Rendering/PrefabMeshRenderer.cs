829dfcbf5d56a2eeeef73903de2b2a7b8ff42c04
Eitrum <sebbbe97@hotmail.com>
2019-03-01 21:48:50 +0100

    Rendering tests

---
+++ Rendering/PrefabMeshRenderer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eitrum.Rendering {
+    public class PrefabMeshRenderer : EiComponent {
+
+        #region Variables
+
+        [SerializeField] private GameObject renderTarget;
+
+        private RenderingBlock[] renderingBlocks;
+
+        #endregion
+
+        #region Properties
+
+        public GameObject RenderTarget {
+            get {
+                return renderTarget;
+            }
+        }
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake() {
+            Assign(renderTarget);
+        }
+
+        private void OnEnable() {
+            SubscribeLateUpdate();
+        }
+
+        private void OnDisable() {
+            UnsubscribeLateUpdate();
+        }
+
+        #endregion
+
+        #region Assign
+
+        public void Assign(GameObject target) {
+            this.renderTarget = target;
+            var renderers = target.GetComponentsInChildren<Renderer>();
+            renderingBlocks = new RenderingBlock[renderers.Length];
+            for (int i = 0, length = renderers.Length; i < length; i++) {
+                renderingBlocks[i] = RenderingBlock.CreateInstance(renderers[i]);
+            }
+        }
+
+        #endregion
+
+        #region Rendering
+
+        public override void LateUpdateComponent(float time) {
+            Render(transform);
+        }
+
+        public void Render() {
+            for (int i = renderingBlocks.Length - 1; i >= 0; i--) {
+                renderingBlocks[i].Render();
+            }
+        }
+
+        public void Render(Transform transform) {
+            for (int i = renderingBlocks.Length - 1; i >= 0; i--) {
+                renderingBlocks[i].Render(transform.localToWorldMatrix);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Rendering/RenderingBlock.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+
+namespace Eitrum.Rendering {
+    [Serializable]
+    public struct RenderingBlock {
+
+        #region Variables
+
+        public Mesh mesh;
+        public Material material;
+        public Matrix4x4 matrix;
+
+        private static Matrix4x4[] instanceRenderering = new Matrix4x4[8];
+
+        #endregion
+
+        #region Constructor
+
+        public RenderingBlock(Mesh mesh) {
+            this.mesh = mesh;
+            this.material = null;
+            this.matrix = Matrix4x4.identity;
+        }
+
+        public RenderingBlock(Mesh mesh, Material material) {
+            this.mesh = mesh;
+            this.material = material;
+            this.matrix = Matrix4x4.identity;
+        }
+
+        public RenderingBlock(Mesh mesh, Material material, Matrix4x4 matrix) {
+            this.mesh = mesh;
+            this.material = material;
+            this.matrix = matrix;
+        }
+
+        #endregion
+
+        #region Static Create
+
+        public static RenderingBlock CreateInstance(GameObject target) {
+            var rend = target.GetComponentInChildren<Renderer>();
+            if (rend == null)
+                return default(RenderingBlock);
+            if (rend is SkinnedMeshRenderer) {
+                var smr = rend as SkinnedMeshRenderer;
+                return new RenderingBlock(smr.sharedMesh, smr.sharedMaterial, smr.transform.localToWorldMatrix);
+            }
+            var mf = rend.GetComponent<MeshFilter>();
+            if (mf == null)
+                return default(RenderingBlock);
+
+            return new RenderingBlock(mf.sharedMesh, (rend as MeshRenderer).sharedMaterial, rend.transform.localToWorldMatrix);
+        }
+
+        public static RenderingBlock CreateInstance(Renderer renderer) {
+            if (renderer == null)
+                return default(RenderingBlock);
+            if (renderer is SkinnedMeshRenderer) {
+                var smr = renderer as SkinnedMeshRenderer;
+                return new RenderingBlock(smr.sharedMesh, smr.sharedMaterial, smr.transform.localToWorldMatrix);
+            }
+            var mf = renderer.GetComponent<MeshFilter>();
+            if (mf == null)
+                return default(RenderingBlock);
+
+            return new RenderingBlock(mf.sharedMesh, (renderer as MeshRenderer).sharedMaterial, renderer.transform.localToWorldMatrix);
+        }
+
+        #endregion
+
+        #region Render
+
+        public void Render() {
+            Graphics.DrawMesh(mesh, matrix, material, 1);
+        }
+
+        public void Render(Material overrideMaterial) {
+            Graphics.DrawMesh(mesh, matrix, overrideMaterial, 1);
+        }
+
+        public void Render(Matrix4x4 matrix) {
+            Graphics.DrawMesh(mesh, matrix * this.matrix, material, 1);
+        }
+
+        public void Render(Matrix4x4 matrix, Material overrideMaterial) {
+            Graphics.DrawMesh(mesh, matrix * this.matrix, overrideMaterial, 1);
+        }
+
+        public void Render(Matrix4x4[] matrices) {
+            var mLength = matrices.Length;
+            if (instanceRenderering.Length < mLength) {
+                var tLenght = instanceRenderering.Length;
+                do {
+                    tLenght *= 2;
+                } while (tLenght < mLength);
+                instanceRenderering = new Matrix4x4[tLenght];
+            }
+            for (int i = 0; i < mLength; i++) {
+                instanceRenderering[i] = matrices[i] * this.matrix;
+            }
+            Graphics.DrawMeshInstanced(mesh, 0, material, matrices, mLength);
+        }
+
+        #endregion
+    }
+}
